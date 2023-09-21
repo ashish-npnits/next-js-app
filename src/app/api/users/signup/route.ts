dd29b88a65b0487bba90becb286a3a1137b378fc
Ashish Kumar <ashish@npnits.com>
2023-09-21 16:25:15 +0530

    verify email logic using token

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,4 +1,5 @@
 import connect from "@/dbConfig/dbConfig";
+import { sendEmail } from "@/helpers/mailer";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 var bcrypt = require('bcryptjs');
@@ -29,6 +30,8 @@ export async function POST(request: NextRequest) {
 
         const savedUser = await newUser.save()
         console.log(savedUser);
+
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id});
 
         return NextResponse.json({
             message: "User created successfully",
